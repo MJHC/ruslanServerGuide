7b0e661689c042e6113aa54e9e1073a4528075d6
MJHC <magnus@bonusfamilien.dk>
2021-10-15 11:58:25 +0200

    now using public folder

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
-const port = 443;
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',(req, res)=>{
-    res.send('Hello World!');
+    res.send(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(port, () => console.log('App runnig'));
